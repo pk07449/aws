fca31376380eca3da17a55f8276f7fbad6bf8b28
Pankaj kumar <java.j2ee.pankaj@gmail.com>
2018-11-15 06:23:09 +0530

    added cloud formation for sns creation.

---
+++ lambda/with-sns/src/main/java/com/pankaj/aws/java/lambda/requestMethodHandler/RequestHandlerLambda.java
@@ -0,0 +1,23 @@
+package com.pankaj.aws.java.lambda.requestMethodHandler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+/**
+ * Created by pankaj on 11/2/2018.
+ */
+public class RequestHandlerLambda implements RequestHandler<Request,Response> {
+
+    public Response handleRequest(Request request, Context context) {
+
+        Response response = new Response();
+        response.setId(100 + request.getId());
+        response.setName("Hello "+request.getName());
+
+        System.out.println("RequestHandlerLambda start");
+        System.out.println(response);
+        System.out.println("RequestHandlerLambda end");
+
+        return response;
+    }
+}

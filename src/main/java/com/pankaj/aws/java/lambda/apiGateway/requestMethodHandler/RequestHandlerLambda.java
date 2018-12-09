0b2e1a6c5cd4749e3d97c4307357b363b4b27581
Pankaj kumar <java.j2ee.pankaj@gmail.com>
2018-12-09 08:42:39 +0530

    lambda integration initial commit with dynamodb,rds,sqs,sns,s3,kms and api-gatewary.

---
+++ src/main/java/com/pankaj/aws/java/lambda/apiGateway/requestMethodHandler/RequestHandlerLambda.java
@@ -0,0 +1,21 @@
+package com.pankaj.aws.java.lambda.apiGateway.requestMethodHandler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+/**
+ * Created by pankaj on 11/2/2018.
+ */
+
+public class RequestHandlerLambda implements RequestHandler<Request,Response> {
+    public Response handleRequest(Request request, Context context) {
+        Response response = new Response();
+        response.setId(100 + request.getId());
+        response.setName("Hello "+request.getName());
+
+        System.out.println("com.pankaj.aws.java.lambda.apiGateway.requestMethodHandler.RequestHandlerLambda.handleRequest start");
+        System.out.println(response);
+        System.out.println("com.pankaj.aws.java.lambda.apiGateway.requestMethodHandler.RequestHandlerLambda.handleRequest end");
+        return response;
+    }
+}
